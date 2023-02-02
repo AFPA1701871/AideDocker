d1127b2d804e5dafdcd79529dca2550cadd5381e
AFPA1701871 <martine.poix@afpa.fr>
2023-02-02 10:51:07 +0100

    Update nettoyage.sh

--- php_mysql/nettoyage.sh
+++ php_mysql/nettoyage.sh
@@ -1,5 +1,12 @@
-
+# on arrete les serveurs
+docker stop server-web
+docker stop server-php
+docker stop server-phpmyadmin
+docker stop server-mysql
+# on suprime les images
 docker image  rm php_mysql-server-web
 docker image  rm php_mysql-server-mysql
+# on vide le dossier contenant la base de données
 rmdir mysqldata /S /Q
+# on lance l'application
 docker-compose up -d
